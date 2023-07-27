d31ee402fabe9d4cda2d447d0bbf693e28451a2f
tayc23 <tayc13@list.ru>
2023-07-27 10:12:02 +0300

    first exercise

---
+++ 02_prime_numbers.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+
+# Есть функция генерации списка простых чисел
+
+
+def get_prime_numbers(n):
+    prime_numbers = []
+    for number in range(2, n+1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+    return prime_numbers
+
+# Часть 1
+# На основе алгоритма get_prime_numbers создать класс итерируемых обьектов,
+# который выдает последовательность простых чисел до n
+#
+# Распечатать все простые числа до 10000 в столбик
+
+
+class PrimeNumbers:
+        def __init__(self, n):
+            self.n = n
+
+        def __iter__(self):
+            self.number = 2
+            self.prime_numbers = []
+            return self
+
+        def __next__(self):
+            while self.number <= self.n:
+                for prime in self.prime_numbers:
+                    if self.number % prime == 0:
+                        break
+                else:
+                    self.prime_numbers.append(self.number)
+                    result = self.number
+                    self.number += 1
+                    return result
+                self.number += 1
+            raise StopIteration
+
+
+# prime_number_iterator = PrimeNumbers(10000)
+# for number in prime_number_iterator:
+#     print(number)
+
+
+
+# Часть 2
+# Теперь нужно создать генератор, который выдает последовательность простых чисел до n
+# Распечатать все простые числа до 10000 в столбик
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def primes_generator(n):
+    for num in range(2, n):
+        if is_prime(num):
+            yield num
+
+n = 10000
+primes = primes_generator(n)
+for prime in primes:
+    print(prime)
+
+# Часть 3
+# Написать несколько функций-фильтров, которые выдает True, если число:
+# 1) "счастливое" в обыденном пониманиии - сумма первых цифр равна сумме последних
+#       Если число имеет нечетное число цифр (например 727 или 92083),
+#       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
+#           727 -> 7(2)7 -> 7 == 7 -> True
+#           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
+# 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
+# 3) придумать свою (https://clck.ru/GB5Fc в помощь)
+#
+# Подумать, как можно применить функции-фильтры к полученной последовательности простых чисел
+# для получения, к примеру: простых счастливых чисел, простых палиндромных чисел,
+# простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
+#
+# Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
